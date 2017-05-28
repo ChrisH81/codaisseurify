91e78f69023d091ac029653ffdf51d319ad7827d
ChrisH81 <chrishurst1981@icloud.com>
2017-05-28 20:57:18 +0200

    Added more artists to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,44 @@
 
 mastodon = Artist.create!( { name: "Mastodon", bio: "Atlanta-based sludge/stoner/alternative metal outfit Mastodon formed in 1999 around the talents of guitarist Bill Kelliher, drummer Bränn Dailor, bassist/vocalist Troy Sanders, and guitarist/vocalist Brent Hinds. One of the more notable New Wave of American Heavy Metal acts, a genre spawned in the mid-'90s by bands like Pantera, Biohazard, and Machine Head, Mastodon's innovative, lyrically astute blend of progressive metal, grindcore, and hardcore helped position the band as one of the preeminent metal acts of the early 21st century.", image_url: "https://goo.gl/Y8Xmoe" } )
 
+deftones = Artist.create!( { name: "Deftones", bio: "Deftones is an American alternative metal band from Sacramento, California, U.S. Formed in 1988, the band was founded by Chino Moreno (lead vocals, rhythm guitar), Stephen Carpenter (lead guitar), Abe Cunningham (drums) and Dominic Garcia (bass). During the group's first five years, the band's lineup changed several times, but stabilized in 1993 when Cunningham rejoined the group after his departure in 1990. Cunningham replaced John Taylor, who had replaced Garcia after he had switched to drums in 1990, and the bassist role had been filled by Chi Cheng. The lineup remained stable for fifteen years, with the exception of keyboardist and turntablist Frank Delgado being added in 1999.", image_url: "https://goo.gl/1G7uh2" } )
+
+alice_in_chains = Artist.create!( { name: "Alice In Chains", bio: "Alice in Chains is an American rock band formed in Seattle, Washington, in 1987 by guitarist and songwriter Jerry Cantrell and original lead vocalist Layne Staley. The initial lineup was rounded out by drummer Sean Kinney and bassist Mike Starr, who was replaced in 1993 by Mike Inez.
+
+Although widely associated with grunge music, the band's sound incorporates heavy metal elements. Since its formation, Alice in Chains has released five studio albums, three EPs, two live albums, four compilations, and two DVDs. The band is known for its distinctive vocal style, which often included the harmonized vocals of Staley and Cantrell (and later William DuVall).", image_url: "https://goo.gl/ZSnpQ9" } )
+
+soundgarden = Artist.create!( { name: "Soundgarden", bio: "Soundgarden is an American rock band formed in Seattle, Washington, in 1984 by singer and rhythm guitarist Chris Cornell, lead guitarist Kim Thayil, and bassist Hiro Yamamoto. Matt Cameron became the band's full-time drummer in 1986, while bassist Ben Shepherd became a permanent replacement for Yamamoto in 1990. Cornell remained in Soundgarden until his death in May 2017, putting the band's future in doubt and leaving Thayil as the only remaining original member of the band.", image_url: "https://goo.gl/AxMlb7" } )
+
+pearl_jam = Artist.create!( { name: "Pearl Jam", bio: "Pearl Jam is an American rock band formed in Seattle, Washington, in 1990. Since its inception, the band's line-up has comprised Eddie Vedder (lead vocals), Mike McCready (lead guitar), Stone Gossard (rhythm guitar) and Jeff Ament (bass). The band's fifth member is drummer Matt Cameron (also of Soundgarden), who has been with the band since 1998. Boom Gaspar (piano) has also been a session/touring member with the band since 2002. Drummers Dave Krusen, Matt Chamberlain, Dave Abbruzzese and Jack Irons are former members of the band.", image_url: "https://goo.gl/T1aOB6" } )
+
+foo_fighters = Artist.create!( { name: "Foo Fighters", bio: "Foo Fighters is an American rock band, formed in Seattle, Washington in 1994. It was founded by Nirvana drummer Dave Grohl as a one-man project following the dissolution of Nirvana after the death of Kurt Cobain. The group got its name from the UFOs and various aerial phenomena that were reported by Allied aircraft pilots in World War II, which were known collectively as foo fighters.", image_url: "https://goo.gl/S843HE" } )
+
+incubus = Artist.create!( { name: "Incubus", bio: "An incubus is a Lilin-demon in male form who, according to mythological and legendary traditions, lies upon women in order to engage in sexual activity with them. Its female counterpart is a succubus. Salacious tales of incubi and succubi have been told for many centuries in traditional societies. Some traditions hold that repeated sexual activity with an incubus or succubus may result in the deterioration of health, or even death.", image_url: "https://goo.gl/zWA68F" } )
+
+weezer = Artist.create!( { name: "Weezer", bio: "Weezer is an American rock band formed in Los Angeles in 1992, consisting of Rivers Cuomo (lead vocals, guitar), Patrick Wilson (drums), Brian Bell (guitar, backing vocals, keyboards), and Scott Shriner (bass guitar, backing vocals). Weezer has sold 9.2 million albums in the US and over 17 million worldwide", image_url: "https://goo.gl/VAkycA" } )
+
+everclear = Artist.create!( { name: "Everclear", bio: "Everclear is an American rock band formed in Portland, Oregon in 1991.[1] The band was formed by Art Alexakis, the band's lead songwriter, vocalist, and guitarist; and for most of the band's height of popularity, consisted of Craig Montoya on bass guitar and Greg Eklund on drums. After the limited release of their independently released debut album, World of Noise, the band found success with their first three albums on Capitol Records: Sparkle and Fade, So Much for the Afterglow, and Songs from an American Movie Vol. One: Learning How to Smile,[1] which were all certified platinum in sales. However, the following two albums Songs from an American Movie Vol. Two: Good Time for a Bad Attitude and Slow Motion Daydream, were not as well received, and as sales suffered, Montoya and Eklund left the band shortly after in 2003.", image_url: "http://fazewp.fazemediainc.netdna-cdn.com/cms/wp-content/uploads/2014/10/everclear-1.jpg" } )
+# Ash = Artist.create!( { name: "Ash", bio: "", image_url: "" } )
+# Radiohead = Artist.create!( { name: "Radiohead", bio: "", image_url: "" } )
+# The Smith Street Band = Artist.create!( { name: "The Smith Street Band", bio: "", image_url: "" } )
+# Undeclinable Ambuscade = Artist.create!( { name: "Undeclinable Ambuscade", bio: "", image_url: "" } )
+# Strung Out = Artist.create!( { name: "Strung Out", bio: "", image_url: "" } )
+# Lagwagon = Artist.create!( { name: "Lagwagon", bio: "", image_url: "" } )
+# NOFX = Artist.create!( { name: "NOFX", bio: "", image_url: "" } )
+# No Use For A Name = Artist.create!( { name: "No Use For A Name", bio: "", image_url: "" } )
+# Pulley = Artist.create!( { name: "Pulley", bio: "", image_url: "" } )
+# Joy Division = Artist.create!( { name: "Joy Division", bio: "", image_url: "" } )
+# Snarky Puppy = Artist.create!( { name: "Snarky Puppy", bio: "", image_url: "" } )
+# Avalanche City = Artist.create!( { name: "Avalanche City", bio: "", image_url: "" } )
+# Queen = Artist.create!( { name: "Queen", bio: "", image_url: "" } )
+# The Temper Trap = Artist.create!( { name: "The Temper Trap", bio: "", image_url: "" } )
+# Architects = Artist.create!( { name: "Architects", bio: "", image_url: "" } )
+# The Dillinger Escape Plan = Artist.create!( { name: "The Dillinger Escape Plan", bio: "", image_url: "" } )
+
 
 artists_count = Artist.all.length
 puts "#{artists_count} artists were created."
 
-deftones = Artist.create!( { name: "Deftones", bio: "Deftones is an American alternative metal band from Sacramento, California, U.S. Formed in 1988, the band was founded by Chino Moreno (lead vocals, rhythm guitar), Stephen Carpenter (lead guitar), Abe Cunningham (drums) and Dominic Garcia (bass). During the group's first five years, the band's lineup changed several times, but stabilized in 1993 when Cunningham rejoined the group after his departure in 1990. Cunningham replaced John Taylor, who had replaced Garcia after he had switched to drums in 1990, and the bassist role had been filled by Chi Cheng. The lineup remained stable for fifteen years, with the exception of keyboardist and turntablist Frank Delgado being added in 1999.", image_url: "https://goo.gl/1G7uh2" } )
 
 Album.create!([
   { album_name: "Adrenaline", released: 1995, album_image_url: "https://goo.gl/vMO6Og", artist: deftones },
@@ -18,6 +51,7 @@ Album.create!([
   { album_name: "Diamond Eyes", released: 2010, album_image_url: "https://goo.gl/n1kxKI", artist: deftones },
   { album_name: "Koi No Yokan", released: 2012, album_image_url: "https://goo.gl/gSfGmQ", artist: deftones },
   { album_name: "Gore", released: 1995, album_image_url: "https://goo.gl/7gJNFY", artist: deftones },
+  { album_name: "Emperor of Sand", released: 2017, album_image_url: "https://goo.gl/1QauEl", artist: mastodon },
 ])
 
 albums_count = Album.all.length
